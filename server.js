ea68c4319451b045ce9566d2a765f60224945d5c
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-17 16:49:32 +0200

    full stack

---
+++ server.js
@@ -0,0 +1,24 @@
+// const express = require('express') // common js
+import express from 'express';
+import cors from 'cors';
+const app = express();
+
+app.use(cors());
+
+const arr = [1, 2, 3, 4];
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1>');
+});
+
+app.get('/posts', (req, res) => {
+  res.status(200).json(arr);
+});
+
+app.get('*', (req, res) => {
+  res.status(404).json({ error: 'not found' });
+});
+
+app.listen(3000, () => {
+  console.log('backas sukasi and port localhost:3000');
+});
